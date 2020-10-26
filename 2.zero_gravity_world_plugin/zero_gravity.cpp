d4c134cee8ebb37bb39b0856f4d16d8bb2c78392
Jenny <jaeeun.kje@gmail.com>
2020-10-26 12:54:14 +0900

    Update zero_gravity.cpp

--- 2.zero_gravity_world_plugin/zero_gravity.cpp
+++ 2.zero_gravity_world_plugin/zero_gravity.cpp
@@ -5,13 +5,11 @@ namespace gazebo
 {
   class ZeroGravityPlugin : public WorldPlugin
   {
-    public: ZeroGravityPlugin() : WorldPlugin()
-    {}
-
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
             {
-                // Todo
-                // SetGravity() 함수이용하여 gravity 0으로 설정
+              // Todo
+              // SetGravity() 함수이용하여 gravity 0으로 설정
+
             }
   };
   GZ_REGISTER_WORLD_PLUGIN(ZeroGravityPlugin)
